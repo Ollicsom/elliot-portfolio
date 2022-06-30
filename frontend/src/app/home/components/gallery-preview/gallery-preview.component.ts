a37c92df8e91d2f26963efd48eddba3932cb019d
Léo Moscillo <leo.moscillo@anthropi.fr>
2022-06-30 13:56:45 +0200

    Randomizing rotation of gallery card + setting position

--- frontend/src/app/home/components/gallery-preview/gallery-preview.component.ts
+++ frontend/src/app/home/components/gallery-preview/gallery-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery-preview',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryPreviewComponent implements OnInit {
 
-  constructor() { }
+  index2: number = 0;
+  position: string = '';
 
+  @Input() set index(value: number) {
+    if (value % 2 === 0) {
+      this.position = 'left'
+    } else {
+      this.position = 'right'
+    }
+}
+  rotation = 0;
+  
+  constructor() { }
+  
   ngOnInit(): void {
+    this.rotation = Math.round(Math.random());
   }
 
 }
